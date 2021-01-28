5efab2fdbf780f11cb2f8015f5d45d780e135ece
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-28 18:45:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,10 @@
 
 
 def key_for_min_value(name_hash)
-  smallest_value = 0
-  empty_hash = nil
+smallest_value = 0
   name_hash.each do |key, value|
     if smallest_value == 0 || value < smallest_value
       smallest_value = value
-      empty_hash = name
   end
-end
-empty_hash
+
 end
